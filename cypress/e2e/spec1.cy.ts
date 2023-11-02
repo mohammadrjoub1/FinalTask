f650229b23fe42480f577ebd74fc7826fd04f4be
Your Name <you@example.com>
2023-11-02 22:54:02 +0200

    creating the master Branch

---
+++ cypress/e2e/spec1.cy.ts
@@ -0,0 +1,9 @@
+import { faker } from "@faker-js/faker";
+
+describe("Senario #1", () => {
+  beforeEach("senarion #1", () => {
+    cy.login("Admin", "admin123");
+  });
+
+  it("TC #1", () => {});
+});
