2ecde0ba1c0fbdfe1184917f7495ddcfb0f62448
Your Name <you@example.com>
2023-11-05 17:47:38 +0200

    phaseTwo Almost one

--- cypress/e2e/spec1.cy.ts
+++ cypress/e2e/spec1.cy.ts
@@ -8,9 +8,6 @@ import { Claim } from "../support/helpers/claim";
 describe("Senario #1", () => {
   beforeEach("senarion #1", () => {
     cy.logIn("Admin", "admin123");
-  });
-
-  it.only("Admin should be able to submit(Pay) a claim", () => {
     cy.fixture("event").then((data) => {
       Events.createEvent(data.name, data.description);
     });
@@ -25,11 +22,14 @@ describe("Senario #1", () => {
     cy.fixture("claimDetails").then((data) => {
       Claim.createClaim(data.currencyId, data.remarks, data.date, data.amount, data.note);
     });
-    
-    // cy.logOut();
-    // cy.logIn("Admin", "admin123");
-
-    // DeleteData.deleteAll();
   });
-  it("Admin should be able to unaccepted  a claim", () => {});
+  afterEach("", () => {
+    DeleteData.deleteAll();
+  });
+  it("Admin should be able to APPROVE (Pay) a claim", () => {
+    Claim.approveClaim();
+  });
+  it("Admin should be able to Reject a claim", () => {
+    Claim.cancelClaim();
+  });
 });
--- cypress/support/helpers/claim.ts
+++ cypress/support/helpers/claim.ts
@@ -20,24 +20,57 @@ export class Claim {
               amount: "50000.00",
               note: null,
             },
-          }).then(() => {
-            cy.api({
-              method: "PUT",
-              url: `/api/v2/claim/requests/${response.body.data.id}/action`,
-              body: {
-                action: "SUBMIT",
-              },
+          })
+            .then(() => {
+              cy.wrap(response.body.data.id).as(`calimId`);
+            })
+            .then(() => {
+              cy.api({
+                method: "PUT",
+                url: `/api/v2/claim/requests/${response.body.data.id}/action`,
+                body: {
+                  action: "SUBMIT",
+                },
+              });
             });
-          });
         });
       });
     });
   }
-}
+  static approveClaim() {
+    cy.get("@calimId").then((calimId) => {
+      cy.logOut();
+      cy.logIn("Admin", "admin123");
+      cy.api({
+        method: "PUT",
+        url: `/api/v2/claim/requests/${calimId}/action`,
+        body: {
+          action: "APPROVE",
+        },
+      });
+    });
+    Claim.claimAssertion("Paid");
+  }
+  static cancelClaim() {
+    cy.get("@calimId").then((calimId) => {
+      cy.logOut();
+      cy.logIn("Admin", "admin123");
+      cy.api({
+        method: "PUT",
+        url: `/api/v2/claim/requests/${calimId}/action`,
+        body: {
+          action: "REJECT",
+        },
+      });
+    });
+     Claim.claimAssertion("Rejected");
+  }
 
-// {
-//   expenseTypeId: expenseId,
-//   date: date,
-//   amount: amount,
-//   note: note,
-// }
+  static claimAssertion(status) {
+    cy.get(":nth-child(11) > .oxd-main-menu-item > .oxd-text").click();
+
+    cy.get(".oxd-table-body").contains("div", "mohammad rjoub").parent().parent().find("div").eq(10).should("contain", "2023-11-05");
+    cy.get(".oxd-table-body").contains("div", "mohammad rjoub").parent().parent().find("div").eq(12).should("contain", status);
+    cy.get(".oxd-table-body").contains("div", "mohammad rjoub").parent().parent().find("div").eq(14).should("contain", "50,000.00");
+  }
+}
